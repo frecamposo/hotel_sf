238002dc8a2a251d1219fee885441309539f3efd
frecamposo <42188039+frecamposo@users.noreply.github.com>
2025-11-04 13:44:22 -0300

    Add files via upload

---
+++ inicio.bat
@@ -0,0 +1,23 @@
+@echo off
+REM Activa el entorno virtual (reemplaza 'nombre_entorno' con el nombre de tu entorno)
+call entorno\Scripts\activate.bat
+
+REM Navega a la carpeta del proyecto (reemplaza 'nombre_proyecto' con el nombre de tu proyecto)
+cd hotel
+
+REM Ejecuta el servidor Django
+REM echo Iniciando el servidor de Django...
+start chrome --start-fullscreen  "http://localhost:8010/"
+python manage.py runserver 8010
+exit
+
+
+
+
+REM Opcional: puedes incluir comandos adicionales aquí, como ejecutar migraciones
+REM echo Aplicando migraciones...
+REM python manage.py migrate
+REM start chrome --start-maximized  "http://localhost:8005/docs"
+REM start chrome --start-fullscreen "https://www.netflix.com"
+REM uvicorn backend.main:app --port 8005 --reload
+REM start chrome http://localhost:8005/docs
